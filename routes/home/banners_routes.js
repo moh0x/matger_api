d615ed071ddb06f8f9cfeee9ab8c5d5d2d4212e6
moh0x <141264101+moh0x@users.noreply.github.com>
2024-06-09 11:53:18 -0700

    Update banners_routes.js

--- routes/home/banners_routes.js
+++ routes/home/banners_routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { verifyToken } = require("../../utility/verify_token");
-const bannersController = require('../../controllers/home/banners_controllerl');
+const bannersController = require('../../controllers/home/banners_controller');
 router.get('/getAllBanners',verifyToken,bannersController.getAllBanners);
 module.exports = 
 router
